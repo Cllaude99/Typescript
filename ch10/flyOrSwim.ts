b73b8becf34bb1585c9aaca28dda66802dda5a86
TaeYoon Kim <xodbsrla1025@naver.com>
2023-02-02 12:45:21 +0900

    update10

---
+++ ch10/flyOrSwim.ts
@@ -0,0 +1,10 @@
+import {Bird, Fish} from './BirdAndFish'
+
+export const flyOrSwim = (o: Bird | Fish): void => {
+  if(o instanceof Bird){
+    o.fly()
+  }
+  else if(o instanceof Fish){
+    o.swim()
+  }
+}
